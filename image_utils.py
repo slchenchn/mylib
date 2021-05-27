6a1cfaaaf1d9e1b714938132e003e4a964150da6
unknown <1106982578@qq.com>
2021-05-27 11:03:49 +0800

    revise image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -19,3 +19,13 @@ def save_cv2_image_as_chinese_path(img, dst_path):
 	'''
 	ext_name = osp.splitext(dst_path)[1]
 	cv2.imencode(ext_name, img)[1].tofile(dst_path)
+
+
+def read_cv2_image_as_chinese_path(img_path, dtype=np.uint8):
+	''' using cv2 to read image in chinese path
+
+	Args:
+		img_path (str): image path
+		dtype (np.dtype): image save data type. Default: np.uint8
+	'''
+	return cv2.imdecode(np.fromfile(img_path, dtype=np.uint8), -1)
