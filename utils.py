da5032b2cdfc0f2135bb839768af8aee44e3b42e
chenda <1106982578@qq.com>
2021-12-28 10:04:51 +0800

    add log_gpu_info

--- utils.py
+++ utils.py
@@ -1,15 +1,83 @@
 '''
 Author: Shuailin Chen
 Created Date: 2021-10-23
-Last Modified: 2021-10-24
+Last Modified: 2021-12-28
 	content: 
 '''
 
-from gpustat import GPUStatCollection
+import os
+import sys
 import time
 import argparse
 import numpy as np
 from numpy import ndarray
+
+from gpustat import GPUStatCollection
+from blessings import Terminal
+
+
+def log_gpu_info(self, 
+                logger,
+                force_color=False,
+                no_color=False,
+                show_cmd=False,
+                show_user=False,
+                show_pid=False,
+                show_power=None,
+                show_fan_speed=None,
+                gpuname_width=16,
+                show_header=True,
+                eol_char=os.linesep,
+                ):
+        # ANSI color configuration
+        if force_color and no_color:
+            raise ValueError("--color and --no_color can't"
+                             " be used at the same time")
+
+        if force_color:
+            t_color = Terminal(kind='linux', force_styling=True)
+
+            # workaround of issue #32 (watch doesn't recognize sgr0 characters)
+            t_color.normal = u'\x1b[0;10m'
+        elif no_color:
+            t_color = Terminal(force_styling=None)
+        else:
+            t_color = Terminal()   # auto, depending on isatty
+
+        # appearance settings
+        entry_name_width = [len(g.entry['name']) for g in self]
+        gpuname_width = max([gpuname_width or 0] + entry_name_width)
+
+        # header
+        if show_header:
+            time_format = locale.nl_langinfo(locale.D_T_FMT)
+
+            header_template = '{t.bold_white}{hostname:{width}}{t.normal}  '
+            header_template += '{timestr}  '
+            header_template += '{t.bold_black}{driver_version}{t.normal}'
+
+            header_msg = header_template.format(
+                    hostname=self.hostname,
+                    width=gpuname_width + 3,  # len("[?]")
+                    timestr=self.query_time.strftime(time_format),
+                    driver_version=self.driver_version,
+                    t=t_color,
+                )
+
+            logger.info(header_msg.strip())
+            logger.info(eol_char)
+
+        # body
+        for g in self:
+            g.print_to(fp,
+                       show_cmd=show_cmd,
+                       show_user=show_user,
+                       show_pid=show_pid,
+                       show_power=show_power,
+                       show_fan_speed=show_fan_speed,
+                       gpuname_width=gpuname_width,
+                       term=t_color)
+            logger.info(eol_char)
 
 
 def wait_for_gpu(required_GB, interval=10):
