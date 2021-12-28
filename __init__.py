10469b78b1899714a18397680b917bce5ed50bc4
chenda <1106982578@qq.com>
2021-12-28 10:50:42 +0800

    n

--- __init__.py
+++ __init__.py
@@ -11,13 +11,13 @@ Last Modified: 2021-12-28
 # from . import mykml
 # from . import image_utils
 
-from mylib import types
-from mylib import utils
-from mylib import labelme_utils
-from mylib import PolSAR
-from mylib import labelme_utils
-from mylib import image_utils
-from mylib import my_torch_tools
-from mylib import file_utils
-from mylib import mykml
-from mylib import types
+from .mylib import types
+from .mylib import utils
+from .mylib import labelme_utils
+from .mylib import PolSAR
+from .mylib import labelme_utils
+from .mylib import image_utils
+from .mylib import my_torch_tools
+from .mylib import file_utils
+from .mylib import mykml
+from .mylib import types
